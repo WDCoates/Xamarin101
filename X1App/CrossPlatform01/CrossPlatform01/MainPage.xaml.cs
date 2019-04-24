7f71c0c506e431cdba52a73539726603c9ca8677
DCoates <DCoates@Rubiconsoftware.com>
2019-04-24 08:07:26 +0100

    First App Simple and Quick

---
+++ X1App/CrossPlatform01/CrossPlatform01/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossPlatform01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button) sender).Text = $"Clicked {count} times! Click Me Again!";
+        }
+    }
+}
