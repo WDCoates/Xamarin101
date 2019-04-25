171d87eda616fc79a13641526e8849dae0a9b9e0
DCoates <DCoates@Rubiconsoftware.com>
2019-04-25 08:11:59 +0100

    Working Single Page App

---
+++ Notes/Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly string _fileName =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MyNotes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+
+            editor.Text = string.Empty;
+        }
+    }
+}
