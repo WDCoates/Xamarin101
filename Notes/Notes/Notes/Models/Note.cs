b044d51650fc10c1598362db796bb98e973c72d2
DCoates <DCoates@Rubiconsoftware.com>
2019-05-02 09:25:30 +0100

    Trying to add a Subject to the Note!

--- Notes/Notes/Notes/Models/Note.cs
+++ Notes/Notes/Notes/Models/Note.cs
@@ -8,6 +8,7 @@ namespace Notes.Models
         [PrimaryKey, AutoIncrement]
         public int Id {  get; set; }
         //public string Filename { get; set;} 
+        public string Subject { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
     }
