f4a65a776e4fb829997a7bf3856149f8093ded46
DCoates <DCoates@Rubiconsoftware.com>
2019-05-21 08:54:52 +0100

    Doing it through code....

---
+++ LayoutWithCode/LayoutWithCode/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutWithCode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            StackLayout stackLayout01 = new StackLayout
+            {
+                Spacing = 0,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "This is done done through code!",
+                        HorizontalOptions = LayoutOptions.Start
+                    }
+                }
+            };
+
+            stackLayout01.Children.Add(new Label
+            {
+                Text = "This is also done done through code!",
+                HorizontalOptions = LayoutOptions.End,
+                BackgroundColor = Color.Yellow
+            });
+
+            this.Content = stackLayout01;
+        }
+    }
+}
