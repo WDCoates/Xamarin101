c2cdb5de6fd10fbf95cf485f9aaba5b27c438661
DCoates <DCoates@Rubiconsoftware.com>
2019-05-23 15:29:21 +0100

    Add Types Entry and Editor

--- Buttons/Buttons/MainPage.xaml.cs
+++ Buttons/Buttons/MainPage.xaml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace Buttons
 {
     public partial class MainPage : ContentPage
     {
+        private string _Text;
+
         public MainPage()
         {
             InitializeComponent();
@@ -37,12 +40,83 @@ namespace Buttons
             sL.Children.Add(btn1);
 
             this.Content = sL;
+
+            Entry e1 = new Entry
+            {
+                Placeholder = "Enter Text Here..."
+            };
+
+            sL.Children.Add(e1);
+            e1.IsPassword = true;
+            e1.TextChanged += e1_TextChanged;
+            e1.Completed += e1_OnEntryCompleted;
+
+
+            Editor m1 = new Editor
+            {
+                Placeholder = "..."
+            };
+
+            m1.HeightRequest = 100;
+            m1.TextChanged += e1_TextChanged;
+            sL.Children.Add(m1);
+
+
+            //https://commons.wikimedia.org/wiki/Main_Page useful source of free to use images.
+
+            Image i1 = new Image
+            {
+                HeightRequest = 200,
+                Source =
+                    "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"                
+            };
+
+            //i1.WidthRequest = 
+
+            sL.Children.Add(i1);
+
+            switch (Device.RuntimePlatform)
+
+            {
+
+                case Device.iOS:
+
+                    this.BackgroundColor = Color.Black;
+
+                    break;
+
+                case Device.Android:
+
+                    this.BackgroundColor = Color.Red;
+
+                    break;
+
+                
+                default:
+
+                    this.BackgroundColor = Color.Yellow;
+
+                    break;
+
+            }
         }
 
         public void btn_Clicked(object sender, EventArgs ea)
         {
             (sender as Button).Text = "Still need help!";
             Console.WriteLine($"This is a call for help....");
+        }
+
+        public void e1_TextChanged(object sender, TextChangedEventArgs ea)
+        {
+            var o = ea.OldTextValue;
+            var n = ea.NewTextValue;
+        }
+
+        public void e1_OnEntryCompleted(object sender, EventArgs ea)
+        {
+            var t = (sender as Entry).Text;
+            _Text = "";
         }
     }
 }
