3d9f417e60a2028196cb67cd002ab1d2f0cdbae7
DCoates <DCoates@Rubiconsoftware.com>
2019-06-03 20:34:51 +0100

    ListViews

---
+++ ListViews/ListViews/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViews
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Monkey> Monkeys { get; private set;}
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Create New List of monkeys:
+            Monkeys = new List<Monkey>
+            {
+                new Monkey
+                {
+                    Name = "Blue Nosed Baboon",
+                    Location = "Africa & Asia",
+                    //ImageUrl =
+                    //    "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+                }
+            };
+
+
+            BindingContext = this;
+        }
+    }
+}
