2bb2b72b3f1f90d449fa5edc567203df6c670b06
DCoates <DCoates@Rubiconsoftware.com>
2019-06-04 09:18:14 +0100

    Failling to get the Bound List Text to Display!

--- ListViews/ListViews/MainPage.xaml.cs
+++ ListViews/ListViews/MainPage.xaml.cs
@@ -11,22 +11,32 @@ namespace ListViews
     public partial class MainPage : ContentPage
     {
         public IList<Monkey> Monkeys { get; private set;}
+        public String bTextStart { get; }
+        public String bTextEnd { get; }
+        
         public MainPage()
         {
             InitializeComponent();
 
+            bTextStart= "Some Binding Stuff Text!";
             //Create New List of monkeys:
             Monkeys = new List<Monkey>
             {
                 new Monkey
                 {
                     Name = "Blue Nosed Baboon",
-                    Location = "Africa & Asia",
+                    Location = "Africa & Asia"
                     //ImageUrl =
                     //    "http://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+                },
+                new Monkey
+                {
+                   Name = "Black Eyed Baboon",
+                   Location = "NE England"
                 }
             };
 
+            bTextEnd = "SomeMoreBindingText!";
 
             BindingContext = this;
         }
