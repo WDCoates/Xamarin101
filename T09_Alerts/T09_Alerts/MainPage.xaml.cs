9e86c6bc1387e0b34479ae9baaad9d57fbd8b753
DCoates <DCoates@Rubiconsoftware.com>
2019-06-12 08:51:20 +0100

    Displat Alerts and get back responces.

---
+++ T09_Alerts/T09_Alerts/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace T09_Alerts
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPage Current;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Current = this;
+        }
+
+        private async void B1_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "This is an alert.", "Ohi");
+            Current.L1.Text = "B1 has been Clicked";
+        }
+
+        private async void B2_Clicked(object sender, EventArgs e)
+        {
+            bool resp = await DisplayAlert("Save Me?", "Would you like tis saved", "Yes", "No");
+            Current.L1.Text = "Save data: " + resp;
+        }
+    }
+}
