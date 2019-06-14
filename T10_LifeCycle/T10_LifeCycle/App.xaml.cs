ebe2e603a36175d2156181c72de3f23778416320
DCoates <DCoates@Rubiconsoftware.com>
2019-06-14 08:52:07 +0100

    Application Lifecycle simple Start,Sleep and Resume

---
+++ T10_LifeCycle/T10_LifeCycle/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace T10_LifeCycle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+
+    }
+}
