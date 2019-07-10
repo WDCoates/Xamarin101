445ae316562ef29005e79cfe3dfa09e18ef550ee
DCoates <DCoates@Rubiconsoftware.com>
2019-07-10 09:14:25 +0100

    Added ToolKit Library

---
+++ ToolKit/HslColour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToolKit
+{
+    public class HslColour : IMarkupExtension
+    {
+        public HslColour() => A = 1;
+
+        private double A { get; set; }
+        public double H { private get; set; }
+        public double S { private get; set; }
+        public double L { private get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Color.FromHsla(H, S, L, A);
+        }
+
+    }
+}
